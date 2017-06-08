94b25cc4ae30055efd4cbff6b3d4319bbdc1458f
yangx <yangx141220127@gmail.com>
2017-06-08 21:08:47 +0800

    [RelationTable]merge same edge

--- test/RelationTable/MakeRelationTable/src/RTableReducer.java
+++ test/RelationTable/MakeRelationTable/src/RTableReducer.java
@@ -3,6 +3,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by hadoop on 17-6-6.
@@ -15,6 +18,16 @@ public class RTableReducer extends Reducer<Text, Text, Text, Text> {
         StringBuilder out = new StringBuilder();//临时存储输出的value部分
         for(Text v:values){
             out.append(v.toString()+",");
+        }
+        out.setLength(out.length()-1);//去掉最后的“,”
+        String temp = new String(out.toString());
+        String[] all = temp.split(",");
+        Set diffName = new HashSet();
+        for(int i=0; i<all.length; i++)
+            diffName.add(all[i]);
+        out = new StringBuilder();
+        for(Iterator it = diffName.iterator();it.hasNext();){
+            out.append(it.next().toString()+",");
         }
         out.setLength(out.length()-1);//去掉最后的“,”
         context.write(key,new Text(out.toString()));
